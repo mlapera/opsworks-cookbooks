30465b1e383846865e4ad72eece49ea59d8e538e
Michael LaPera <michael@lapera.com>
2013-11-27 11:53:31 -0500

    dry run

--- mcmyadmin/recipes/default.rb
+++ mcmyadmin/recipes/default.rb
@@ -1,16 +1,18 @@
+#src_filename = [:mcmyadmin][:mono][:etczip]
+src_filepath = '/tmp'
+#extract_path = "#{[:mcmyadmin][:mono][:etcdir]}/"
 
-src_filename = [:mcmyadmin][:mono][:etczip]
-src_filepath = "#{Chef::Config['file_cache_path']}/#{src_filename}"
-extract_path = "#{[:mcmyadmin][:mono][:etcdir]}/"
-
-remote_file src_filepath do
-  source "#{node[:mcmyadmin][:remote][:mcmyadminurl]}/#{src_filename}"
-end
+#remote_file src_filepath do
+#  source "#{node[:mcmyadmin][:remote][:mcmyadminurl]}/#{src_filename}"
+#end
 
 bash 'setup_mono' do
   cwd ::File.dirname(src_filepath)
   code <<-EOH
-    mkdir -p #{extract_path}
-    unzip #{src_filename} -d #{extract_path}
-    EOH
+cd /tmp
+wget http://mcmyadmin.com/Downloads/etc.zip
+sudo unzip etc.zip -d /usr/local
+echo 2 | sudo alternatives --config java
+cd /mnt/minecraft/mcmyadmin && screen -dmS  McMyAdmin ./MCMA2_Linux_x86_64
+EOH
 end
